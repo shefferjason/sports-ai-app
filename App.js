c1386235eb809516464b40042345842f5be68c51
shefferjason <shefferjason@gmail.com>
2025-12-07 13:51:36 -0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './screens/HomeScreen';
+import NFLScreen from './screens/NFLScreen';
+import NBAScreen from './screens/NBAScreen';
+import AIScreen from './screens/AIScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="NFL" component={NFLScreen} />
+        <Tab.Screen name="NBA" component={NBAScreen} />
+        <Tab.Screen name="AI" component={AIScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
