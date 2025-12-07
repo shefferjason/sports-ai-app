81b0962566337630112677f2365daeb6f87a2fe1
shefferjason <shefferjason@gmail.com>
2025-12-07 15:22:49 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-// App.js
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>SportsLogic AI is running 🎉</Text>
+      <Text>Sports AI App is Running!</Text>
       <StatusBar style="auto" />
     </View>
   );
