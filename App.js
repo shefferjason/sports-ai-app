63c31ba367810218c0749682cced1823af06e140
shefferjason <shefferjason@gmail.com>
2025-12-07 18:26:24 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sports AI App is Running!</Text>
+      <Text style={styles.text}>Sports AI App is Running!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
